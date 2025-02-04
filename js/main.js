980c6c997e5851aeade98071488edd8faa5b3424
jaboikova <yanis.twink@gmail.com>
2025-02-05 01:13:25 +0200

    Fixed circle speeding up when pressing space

--- js/main.js
+++ js/main.js
@@ -3,56 +3,25 @@ import { Ball,  DrawBall } from './ball.js';
 import { Puddle, DrawPuddle } from './puddle.js';
 import { DrawBricks, createBricks } from './bricks.js'
 
+let isBallMoving = false;
 let gameOver = false; 
 let HP = 3;
 let playerScore = 0;
 let brokenBricksCounter = 0;
+ 
 // Initialize all objects
-const ball = new Ball(350, 412, 5, 1, -1, 'black');
+const ball = new Ball(350, 412, 5, 2, -2, 'black');
 const puddle = new Puddle(330, 420, 40, 10, "white");
 const bricks = createBricks(13, 9, 40, 15, 10, 30, 30, "#611717");
-// const brick = new Brick(30, 30, 40, 15, "#611717", true, 13, 9, 10, 30, 30);
 
 function draw() {
     Field();
     DrawBall(ball);
     DrawPuddle(puddle);
     DrawBricks(bricks);
+    moveCircle(ball);
     collisionDetection(ball, bricks);
-
     showPlayerStats(playerScore, brokenBricksCounter);
-}
-
-function showPlayerStats(score, brokenBricks) {
-    const stats = document.getElementById("stats");
-
-    const hpDiv = document.getElementById("hp");
-    const scoreDiv = document.getElementById("score");
-    const brokenBricksDiv = document.getElementById("bricks");
-    
-    hpDiv.textContent = `Your have ${HP} HP left`;
-    scoreDiv.textContent = `Your score is ${score}`;
-    brokenBricksDiv.textContent = `You have broken ${brokenBricks} bricks`;
-}
-
-function collisionDetection(ball, bricks) {
-    bricks.forEach(brick => {
-        if (brick.visible) {
-            // Check if the ball collides with the brick
-            if (
-                ball.x + ball.radius > brick.x && // Ball's right edge > brick's left edge
-                ball.x - ball.radius < brick.x + brick.width && // Ball's left edge < brick's right edge
-                ball.y + ball.radius > brick.y && // Ball's bottom edge > brick's top edge
-                ball.y - ball.radius < brick.y + brick.height // Ball's top edge < brick's bottom edge
-            ) {
-                // Collision detected
-                playerScore += 200;
-                brokenBricksCounter += 1;
-                brick.visible = false; // Hide the brick
-                ball.dy = -ball.dy; // Reverse the ball's vertical direction
-            }
-        }
-    });
 }
 
 // KEY HANDLERS
@@ -60,7 +29,12 @@ function collisionDetection(ball, bricks) {
 document.addEventListener('keydown', function(event) {
     // Arrow key movement logic
    if (event.code === "Space") {
-       setInterval(() => moveCircle(ball), 1);
+        if (!isBallMoving) {
+            isBallMoving = true;
+            ball.dx = 2;
+            ball.dy = -2;
+        }
+       //setInterval(() => moveCircle(ball), 1);
    }
    
    clearCanvasAndRedraw(ball);  
@@ -92,48 +66,73 @@ function clearCanvasAndRedraw(element) {
 }
 
 // Мяч ударяется о стенки и об доску
-function moveCircle() {
+function moveCircle(ball) {
     if (gameOver) return; 
-    if (ball.x + ball.dx > canvas.width-ball.radius || ball.x + ball.dx < ball.radius) { // Left andt right walls
-        ball.dx = -ball.dx;
-    }   
-    if ( ball.y + ball.dy < ball.radius) { // Top wall
-        ball.dy = -ball.dy;
-    } else if (ball.y + ball.dy > puddle.y - ball.radius) { // Bottom 
-            if (ball.x + ball.radius > puddle.x && ball.x - ball.radius < puddle.x + puddle.width) {
-                if (ball.y = ball.y - puddle.height) {
-                    ball.dy = -ball.dy;
-                    //console.log("Touched puddle");
+    if (isBallMoving) {
+        if (ball.x + ball.dx > canvas.width-ball.radius || ball.x + ball.dx < ball.radius) { // Left andt right walls
+            ball.dx = -ball.dx;
+        }   
+        if ( ball.y + ball.dy < ball.radius) { // Top wall
+            ball.dy = -ball.dy;
+        } else if (ball.y + ball.dy > puddle.y - ball.radius) { // Bottom 
+                if (ball.x + ball.radius > puddle.x && ball.x - ball.radius < puddle.x + puddle.width) {
+                    if (ball.y = ball.y - puddle.height) {
+                        ball.dy = -ball.dy;
+                        //console.log("Touched puddle");
+                    }
+                } else if (ball.y + ball.dy > canvas.height-ball.radius) {
+                    if (HP == 1) {
+                        alert(`Game Over. Your score is ${playerScore}`);
+                        setTimeout(() => document.location.reload(), 1000); // Reload after 1 sec
+                        gameOver = true;
+                    }
+                    HP = HP - 1;
+                    resetBall(ball);
                 }
-            } else if (ball.y + ball.dy > canvas.height-ball.radius) {
-                if (HP == 1) {
-                    alert(`Game Over. Youre score is ${playerScore}`);
-                    setTimeout(() => document.location.reload(), 1000); // Reload after 1 sec
-                    gameOver = true;
-                }
-                HP = HP - 1;
-                resetBall(ball);
-            }
+        }
+        
+        ball.x += ball.dx;
+        ball.y += ball.dy;   
     }
- 
-    ball.x += ball.dx/2;
-    ball.y += ball.dy/2;   
 }
 
 function resetBall(ball) {
     ball.x = 350;
     ball.y = 412;
-    ball.dy = 0;
     ball.dx = 0;
-    document.addEventListener('keydown', function(event) {
-        // Arrow key movement logic
-       if (event.code === "Space") {
-            ball.dy = 2;
-            ball.dx = 2;;
-       }
-       
-    });
+    ball.dy = 0;
+    isBallMoving = false;
+}
+
+function showPlayerStats(score, brokenBricks) {
+    const hpDiv = document.getElementById("hp");
+    const scoreDiv = document.getElementById("score");
+    const brokenBricksDiv = document.getElementById("bricks");
     
+    hpDiv.textContent = `Your have ${HP} HP left`;
+    scoreDiv.textContent = `Your score is ${score}`;
+    brokenBricksDiv.textContent = `You have broken ${brokenBricks} bricks`;
+}
+
+
+function collisionDetection(ball, bricks) {
+    bricks.forEach(brick => {
+        if (brick.visible) {
+            // Check if the ball collides with the brick
+            if (
+                ball.x + ball.radius > brick.x && // Ball's right edge > brick's left edge
+                ball.x - ball.radius < brick.x + brick.width && // Ball's left edge < brick's right edge
+                ball.y + ball.radius > brick.y && // Ball's bottom edge > brick's top edge
+                ball.y - ball.radius < brick.y + brick.height // Ball's top edge < brick's bottom edge
+            ) {
+                // Collision detected
+                playerScore += 200;
+                brokenBricksCounter += 1;
+                brick.visible = false; // Hide the brick
+                ball.dy = -ball.dy; // Reverse the ball's vertical direction
+            }
+        }
+    });
 }
 
 function loop() {
@@ -147,10 +146,6 @@ loop();
 
 // сделано движение доски, нарисован весь канвас,
 // механика мяча - отскакивает от стен и доски, при пролетании мимо доски игра окончен
-// надо сделать механику ломания блоков 
-// пофиксить баг где мяч отскакивает пролетая рядом с доской
-// добавить ХП и Счет игрока
-// возможность поставить на паузу и начать сначала
 
-// C и R надо инициализовать в этом файле
-// все переменные кирпича запихнуть в объект, добавить уровень сложности
+// пофиксить баг где мяч отскакивает пролетая рядом с доской
+// возможность поставить на паузу и начать сначала
